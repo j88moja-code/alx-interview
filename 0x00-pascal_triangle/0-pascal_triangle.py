90967e889dd8860133359f2afb7ba054852736dd
j88moja-code <sitholent+github@gmail.com>
2023-03-24 12:44:46 +0000

    Updated

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,28 +1,19 @@
-def pascal_triangle(n:int):
- 
-    # An auxiliary array to store
-    # generated pascal triangle values
-    arr = [[0 for x in range(n)]
-              for y in range(n)]
- 
-    # Iterate through every line
-    # and print integer(s) in it
-    for line in range (0, n):
- 
-        # Every line has number of
-        # integers equal to line number
-        for i in range (0, line + 1):
- 
-            # First and last values
-            # in every row are 1
-            if(i is 0 or i is line):
-                arr[line][i] = 1
-                print(arr[line][i], end = " ")
- 
-            # Other values are sum of values
-            # just above and left of above
-            else:
-                arr[line][i] = (arr[line - 1][i - 1] +
-                                arr[line - 1][i])
-                print(arr[line][i], end = " ")            
-        print("\n", end = "")
+#!/usr/bin/python3
+'''
+Returns list representing the Pascal's triangle of n
+'''
+
+
+def pascal_triangle(n):
+    '''returns empty list if n <= 0'''
+    pascal_list = []
+    if n <= 0:
+        return pascal_list
+    for i in range(n):
+        row = [1]
+        if pascal_list:
+            last_row = pascal_list[-1]
+            row.extend([sum(pair) for pair in zip(last_row, last_row[1:])])
+            row.append(1)
+        pascal_list.append(row)
+    return (pascal_list)
